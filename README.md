8ad6762efe2beeae059895c8c89cfc4fe3e242c8
Grzesiek <mordimergt@gmail.com>
2018-11-03 21:38:00 +0100

    Creation of TagRecognizer class

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # Parse-input-string-to-array
+
+/**
+ * Class parse data from string - example:
+ * [ input value="asdasd" type="text" name="imie" placeholder="" class="" style="" validators="onlyNumbers, min(3), max(128)" items="1|aaa, 2|bbb" ]
+ * Result getParseTags() = array:
+[
+	'value' => 'asdasd',
+	'type' => 'text',
+	'name' => 'imie',
+	'placeholder' => '',
+	'class' => '',
+	'style' => '',
+	'validators' => [
+	'onlyNumbers' => '',
+	'min' => '3',
+	'max' => '128'
+	],
+	'items' =>[
+		1 => 'aaa',
+		2 => 'bbb'
+	]
+]
+ */
---
+++ TagRecognizer.php
@@ -0,0 +1,121 @@
+<?php
+
+/**
+ * Class parse data from string - example:
+ * [ input value="asdasd" type="text" name="imie" placeholder="" class="" style="" validators="onlyNumbers, min(3), max(128)" items="1|aaa, 2|bbb" ]
+ * Result getParseTags() = array:
+[
+	'value' => 'asdasd',
+	'type' => 'text',
+	'name' => 'imie',
+	'placeholder' => '',
+	'class' => '',
+	'style' => '',
+	'validators' => [
+	'onlyNumbers' => '',
+	'min' => '3',
+	'max' => '128'
+	],
+	'items' =>[
+		1 => 'aaa',
+		2 => 'bbb'
+	]
+]
+ */
+class TagRecognizer {
+
+	/**
+	 *
+	 * @param type $string
+	 */
+	public function __construct(string $string) {
+		$this->string = $string;
+	}
+
+	/**
+	 * get array tags
+	 *
+	 * @return array
+	 */
+	public function getParseTags(): array{
+		//get all between example="example" ::: (\w+) creates array $result[1]-----------([^"]*) creates array $result[2]
+		preg_match_all('@(\w+)="([^"]*)"@', $this->string, $result);
+		//new array created from $result[1](key) => $result[2](Value)
+		$arrayTags = array_combine($result[1], $result[2]);
+
+		return $this->parseTags($arrayTags);
+	}
+
+	/**
+	 * parse tags array to new array
+	 *
+	 * @param array $arrayTags
+	 * @return array
+	 */
+	private function parseTags(array $arrayTags): array{
+
+		$parseArrayTags = [];
+
+		foreach ($arrayTags as $key => $value) {
+			//items
+			if (strpos($value, ',') !== false && strpos($value, '|') !== false) {
+				$parseArrayTags[$key] = $this->parseItems($value);
+				//validators
+			} elseif (strpos($value, ',') !== false) {
+				$parseArrayTags[$key] = $this->parseValidators($value);
+				//simple tag
+			} else {
+				$parseArrayTags[$key] = $value;
+			}
+		}
+
+		return $parseArrayTags;
+	}
+
+	/**
+	 *  parse items in string
+	 *
+	 * @param string $value
+	 * @return array
+	 */
+	private function parseItems(string $value): array{
+		$arrInfo = explode(",", $value);
+
+		$newArray = [];
+		foreach ($arrInfo as $item) {
+			$values = explode("|", $item);
+			$newArray[trim($values[0])] = $values[1];
+		}
+
+		return $newArray;
+	}
+
+	/**
+	 * parse validators in string
+	 *
+	 * @param string $value
+	 * @return array
+	 */
+	private function parseValidators(string $value): array{
+		$arrInfo = explode(",", $value);
+		$newArray = [];
+		foreach ($arrInfo as $item) {
+
+			// if item has () in value - $keyValidatorValue is equal text between this brackets
+			if (strpos($item, '(') !== false && strpos($item, ')') !== false) {
+
+				preg_match('@\((.*?)\)@', $item, $match);
+
+				$keyValidatorValue = $match[1];
+			} else {
+				$keyValidatorValue = '';
+			}
+
+			//$newArray[$item] - delete bracket with text and trim $item
+			$newArray[trim(preg_replace("@\([^)]+\)@", "", $item))] = $keyValidatorValue;
+		}
+
+		return $newArray;
+	}
+
+}
